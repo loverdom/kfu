ae80ff765badfe9caf4918330030abc74d9cee57
loverdom <58025497+loverdom@users.noreply.github.com>
2020-09-10 02:08:55 +0300

    Update and rename lab1.py to teabot_lab.py

--- computer graphics/teabot_lab.py
+++ computer graphics/teabot_lab.py
@@ -2,23 +2,26 @@
 import numpy as np
 import sys
 
+#функция считывания в матрицы
 def to_matrix(filename):
     verge_list = []
     apex_list = []
     with open(filename, 'r') as f:
         for line in f.readlines():
+            #делаем из строки список
+            line = line.split(' ')
             if 'v' in line:
-                line = line.split(' ')
+                #буквы v и f нам ни к чему, поэтому срезаем их
                 apex_list.append(line[1:])
             if 'f' in line:
-                line = line.split(' ')
                 verge_list.append(line[1:])
     verge_matrix = np.array(verge_list, dtype = np.int32)
     apex_matrix = np.array(apex_list, dtype = np.float64)
     return verge_matrix, apex_matrix
 
 def tri_square(a, b, c):
-    s = 0.5*np.abs((b[0] - a[0])*(c[1] - a[1]) - (c[0] - a[0])*(b[1] - a[1]))
+    #площадь через определитель
+    s = 0.5*np.abs((a[0] - c[0])*(b[1] - c[1]) - (b[0] - c[0])*(a[1] - c[1]))
     return s
 
 def total_area(a_matrix, v_matrix):
@@ -26,12 +29,12 @@ def total_area(a_matrix, v_matrix):
     for line in v_matrix:
         list_of_amounts.append(tri_square(a_matrix[line[0] - 1], a_matrix[line[1] - 1],\
                                           a_matrix[line[2] - 1]))
-    sum_array = np.array(list_of_amounts)
-    total_area = np.sum(sum_array)
+    total_area = np.sum(list_of_amounts)
     return total_area
 
 
 def main():
+    #sys.argv[0] имя нашего файла .py
     file_name = sys.argv[1]
     verge_matrix, apex_matrix = to_matrix(file_name)
     area = total_area(apex_matrix, verge_matrix)
@@ -49,7 +52,7 @@ def main():
                                                              np.min(apex_matrix[:, 2])))
     print("|" + 45*"-")
     print("|" + "Third point of lab:")
-    print("|" + "Total area of teapot: {}".format(area))
+    print("|" + "[+] Total area of teapot: {}".format(area))
     print("|" + 45*"-")
 
 if __name__ == "__main__":
